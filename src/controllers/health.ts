71ab5bb8d160c5d1c8437b946e43a76b7dd76782
Luigi Frasca <frasca.luigi@icloud.com>
2020-04-13 11:46:59 +0200

    remove health.ts

--- src/controllers/health.ts
+++
@@ -1,8 +0,0 @@
-
-import * as pkg from '../../package.json';
-import { Request, Response } from 'express';
-import {config} from '../loadConfiguration';
-
-export const getVersionResponse = (req:Request, res:Response) => {
-    res.status(200).send({version: pkg.version, env: config.get('env')})
-}
