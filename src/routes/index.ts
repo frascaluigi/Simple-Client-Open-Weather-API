b29c7d695a8e0690005ecf3c29fc542532dab69c
Luigi Frasca <frasca.luigi@icloud.com>
2020-04-13 11:46:33 +0200

    refactor health controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
-import express from 'express'
-import { getVersionResponse } from '../controllers/health'
+import express from 'express';
+import health from './health';
 import weather from './weather';
 import auth from './auth';
 
 const router = express.Router()
 
-router.use('/health', getVersionResponse);
+router.use('/health', health);
 router.use('/weather', weather);
 router.use('/auth', auth);
 
