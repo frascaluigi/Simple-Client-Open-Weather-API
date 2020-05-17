1436d0c37c9bf36f3bca01c9bc8d44053417678b
Luigi Frasca <frasca.luigi@icloud.com>
2020-05-17 18:24:28 +0200

    add swagger-ui package

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,56 @@
-import express from 'express'
+import express from 'express';
 import morgan from 'morgan';
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import routes from './routes'
-import {config} from './loadConfiguration';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import routes from './routes';
+import { config } from './loadConfiguration';
 import passport from 'passport';
-const {URL} = require('url');
+const { URL } = require('url');
 
-const expressSession =  require('express-session');
+const expressSession = require('express-session');
 const FileStore = require('session-file-store')(expressSession);
 
-const NODE_ENV = config.get('NODE_ENV')
+const NODE_ENV = config.get('NODE_ENV');
 const isDev = NODE_ENV === 'development';
 
-export const app = express()
+export const app = express();
 
-if(isDev){
-  // Use FileStore in development mode
-  app.use(expressSession({
-    resave: false,
-    saveUninitialized: true,
-    secret: 'unguessable',
-    store: new FileStore()
-  }))
-}else{
-  // Use RedisStore in production mode
-  //console.log("REDIS")
+if (isDev) {
+	// Use FileStore in development mode
+	app.use(
+		expressSession({
+			resave: false,
+			saveUninitialized: true,
+			secret: 'unguessable',
+			store: new FileStore(),
+		})
+	);
+} else {
+	// Use RedisStore in production mode
 }
 
 const CONFIG_SERVER = config.get('server');
-const serviceUrl = new URL('/api', CONFIG_SERVER.service_url+':'+CONFIG_SERVER.port);
-  
-app.use(cors())
-app.use(bodyParser.json({ limit: '12mb' }))
-app.use(bodyParser.urlencoded({ extended: false }))
-  
+const serviceUrl = new URL(
+	'/api',
+	CONFIG_SERVER.service_url + ':' + CONFIG_SERVER.port
+);
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '12mb' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(morgan('development'));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 app.use('/api', routes);
 
-NODE_ENV !== 'test' && app.listen(CONFIG_SERVER.port, () => {
-    console.info(`${config.get('app_name')} started: ${serviceUrl} (ENVIRONMENT: ${NODE_ENV})`)
-})
+NODE_ENV !== 'test' &&
+	app.listen(CONFIG_SERVER.port, () => {
+		console.info(
+			`${config.get(
+				'app_name'
+			)} started: ${serviceUrl} (ENVIRONMENT: ${NODE_ENV})`
+		);
+	});
